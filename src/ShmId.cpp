95b1ea3b071a26cdbd8dab42250161573dd7d394
Volker Schwaberow <volker@schwaberow.de>
2025-01-15 06:00:27 +0100

    feat: enhance ShmId class to manage shared memory data and size attributes

--- src/ShmId.cpp
+++ src/ShmId.cpp
@@ -4,16 +4,25 @@
 #include <stdexcept>
 #include <sys/shm.h>
 
-specbolt::ShmId::ShmId(const std::size_t num_bytes) {
+specbolt::ShmId::ShmId(const std::size_t num_bytes) : size_(num_bytes) {
   id_ = shmget(IPC_PRIVATE, num_bytes, IPC_CREAT | 0600);
   if (id_ < 0) {
     throw std::runtime_error(std::format("Failed to create shared memory: {}", errno));
   }
+  data_ = static_cast<char*>(shmat(id_, nullptr, 0));
+  if (data_ == reinterpret_cast<char*>(-1)) {
+    throw std::runtime_error(std::format("Failed to attach shared memory: {}", errno));
+  }
 }
 
 void specbolt::ShmId::reset() {
-  if (id_ != -1)
+  if (data_ != nullptr && data_ != reinterpret_cast<char*>(-1)) {
+    shmdt(data_);
+    data_ = nullptr;
+  }
+  if (id_ != -1) {
     shmctl(id_, IPC_RMID, nullptr);
-  // TODO decide what to do if shmctl fails.
-  id_ = -1;
+    id_ = -1;
+  }
+  size_ = 0;
 }
--- src/ShmId.hpp
+++ src/ShmId.hpp
@@ -7,23 +7,35 @@ namespace specbolt {
 /// Holder for a shared memory id.
 class ShmId {
   int id_{-1};
+  std::size_t size_{0};
+  char* data_{nullptr};
 
 public:
   ShmId() = default;
   explicit ShmId(std::size_t num_bytes);
   ShmId(const ShmId &) = delete;
   ShmId &operator=(const ShmId &) = delete;
-  ShmId(ShmId &&other) noexcept : id_(other.id_) { other.id_ = -1; }
+  ShmId(ShmId &&other) noexcept : id_(other.id_), size_(other.size_), data_(other.data_) {
+    other.id_ = -1;
+    other.size_ = 0;
+    other.data_ = nullptr;
+  }
   ShmId &operator=(ShmId &&other) noexcept {
     if (this != &other) {
       id_ = other.id_;
+      size_ = other.size_;
+      data_ = other.data_;
       other.id_ = -1;
+      other.size_ = 0;
+      other.data_ = nullptr;
     }
     return *this;
   }
   ~ShmId() { reset(); }
   int id() const { return id_; }
   void reset();
+  char* Data() const { return data_; }
+  std::size_t Size() const { return size_; }
 };
 
 } // namespace specbolt
